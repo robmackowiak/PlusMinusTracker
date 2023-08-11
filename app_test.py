2a6e13cba7b98fae872688727c7b7c4a2602e13e
Rob Mackowiak <robert.mackowiak@mlse.com>
2023-08-11 17:05:21 -0400

    Major functional changes
    - Added multi-session state saving based on date
    - Added Invalid player condition
    - Added +/- table toggle for TMU vs away
    - Improved appearance

--- app_test.py
+++ app_test.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
+import numpy as np
+import csv
+import os.path
+import streamlit_toggle as tog
 
-st.title("Basketball Plus-Minus Tracker")
+st.markdown("<h1 style='text-align: center; color: black;'>Plus-Minus Tracker</h1>", unsafe_allow_html=True)
+top_col1,top_col2,top_col3,top_col4 = st.columns([1,1,1,1])
 # Player selection
 homeplayers = ["Haley Fedick", "Zoe Idahosa", "Jayme Foreman", "Sarai Bailey", "Kaillie Hall",'Kait Nichols','Lauryn Meek','Jamilah Christian','Jess Keripe','Player10']
 col1,col2,col3 = st.columns([1,1,1])
@@ -10,47 +15,84 @@ col1.subheader('TMU')
 # Home team selection
 home_team = [player for player in homeplayers if col1.checkbox(f"{player}")]
 
-col2.text('')
-col2.text('')
-
 # Player selection
 awayplayers = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5",'Player6','Player7','Player8','Player9','Player10']
-col1_2,col2_2,col3_2 = st.columns([1,1,1])
-col1_2.subheader('Away')
+col2.subheader('Away')
 # Home team selection
-away_team = [player for player in awayplayers if col1_2.checkbox(f"{player}",key=player)]
+away_team = [player for player in awayplayers if col2.checkbox(f"{player}",key=player)]
+##existing file
+if os.path.isfile(str(date.today())+'TMU_plusminus.csv'):
+    points_data_home = pd.read_csv(str(date.today())+'TMU_plusminus.csv')
+    points_data_away = pd.read_csv(str(date.today())+'Away_plusminus.csv')
+    st.session_state.home_points = st.session_state.get('home_points', {
+        player: points_data_home.loc[points_data_home['Player'] == player, 'Points'].iloc[0]
+        if player in points_data_home['Player'].values
+        else 0  # Use a default value (e.g., 1) if the player is not found
+        for player in homeplayers
+    })
+    st.session_state.away_points = st.session_state.get('away_points', {
+        player: points_data_away.loc[points_data_away['Player'] == player, 'Points'].iloc[0]
+        if player in points_data_away['Player'].values
+        else 0  # Use a default value (e.g., 1) if the player is not found
+        for player in awayplayers
+    })
+else:
+    st.session_state.home_points = st.session_state.get('home_points', {player: 0 for player in homeplayers})
+    st.session_state.away_points = st.session_state.get('away_points', {player: 0 for player in awayplayers})
 
-if col2.button('Add Point Home'):
-    st.session_state.home_points = st.session_state.get('home_points', {player: 0 for player in home_team})  # Retrieve session state
-    for player in home_team:
-        st.session_state.home_points[player] += 1
-    for player in away_team:
-        st.session_state.away_points[player] -= 1
-    home_df = pd.DataFrame(list(st.session_state['home_points'].items()), columns=['Player', 'Points'])
-    home_df.to_csv(str(date.today())+'TMU_plusminus.csv', index=False)
-    away_df = pd.DataFrame(list(st.session_state['away_points'].items()), columns=['Player', 'Points'])
-    away_df.to_csv(str(date.today())+'Away_plusminus.csv', index=False)
+if len(home_team)==5 and len(away_team)==5:
+    if top_col2.button('Add Point TMU'):
+        #st.session_state.home_points = st.session_state.get('home_points', {player: 0 for player in home_team})  # Retrieve session state
+        for player in home_team:
+            st.session_state.home_points[player] += 1
+        for player in away_team:
+            st.session_state.away_points[player] -= 1
+        
+        new_row_data = [home_team[0],home_team[1],home_team[2],home_team[3],home_team[4],1]
+        with open('lineup_plusminus.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(new_row_data)
+        home_df = pd.DataFrame(list(st.session_state['home_points'].items()), columns=['Player', 'Points'])
+        home_df.to_csv(str(date.today())+'TMU_plusminus.csv', index=False)
+        away_df = pd.DataFrame(list(st.session_state['away_points'].items()), columns=['Player', 'Points'])
+        away_df.to_csv(str(date.today())+'Away_plusminus.csv', index=False)
 
-if col2.button('Add Point Away'):
-    st.session_state.away_points = st.session_state.get('away_points', {player: 0 for player in away_team})  # Retrieve session state
-    for player in away_team:
-        st.session_state.away_points[player] += 1
-    for player in home_team:
-        st.session_state.home_points[player] -= 1
-    home_df = pd.DataFrame(list(st.session_state['home_points'].items()), columns=['Player', 'Points'])
-    home_df.to_csv(str(date.today())+'TMU_plusminus.csv', index=False)
-    away_df = pd.DataFrame(list(st.session_state['away_points'].items()), columns=['Player', 'Points'])
-    away_df.to_csv(str(date.today())+'Away_plusminus.csv', index=False)
+    if top_col3.button('Add Point Away'):
+        #st.session_state.away_points = st.session_state.get('away_points', {player: 0 for player in away_team})  # Retrieve session state
+        for player in away_team:
+            st.session_state.away_points[player] += 1
+        for player in home_team:
+            st.session_state.home_points[player] -= 1
+        
+        new_row_data = [home_team[0],home_team[1],home_team[2],home_team[3],home_team[4],-1]
+        with open('lineup_plusminus.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(new_row_data)
 
-st.session_state.home_points = st.session_state.get('home_points', {player: 0 for player in homeplayers})
-st.session_state.away_points = st.session_state.get('away_points', {player: 0 for player in awayplayers})
+        home_df = pd.DataFrame(list(st.session_state['home_points'].items()), columns=['Player', 'Points'])
+        home_df.to_csv(str(date.today())+'TMU_plusminus.csv', index=False)
+        away_df = pd.DataFrame(list(st.session_state['away_points'].items()), columns=['Player', 'Points'])
+        away_df.to_csv(str(date.today())+'Away_plusminus.csv', index=False)
+else:
+    top_col2.button('Invalid Player #',key=0)
+    top_col3.button('Invalid Player #',key=1)
 
-col3.subheader('+/-')
-if 'home_points' in st.session_state:
-    for player, count in st.session_state.home_points.items():
-        col3.write(f"{player}: {count}")
 
-col3_2.subheader('+/-')
-if 'away_points' in st.session_state:
-    for player, count in st.session_state.away_points.items():
-        col3_2.write(f"{player}: {count}")
+with top_col4:
+    table_switch = tog.st_toggle_switch(label="", 
+                    key="Key1", 
+                    default_value=True, 
+                    label_after = False, 
+                    inactive_color = '#11567f', 
+                    active_color="#11567f", 
+                    track_color="#11567f"
+                    )
+
+if table_switch==True:
+    col3.subheader('TMU')
+    if 'home_points' in st.session_state:
+        col3.dataframe(st.session_state.home_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True)
+else:
+    col3.subheader('Away')
+    if 'away_points' in st.session_state:
+        col3.dataframe(st.session_state.away_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True)
