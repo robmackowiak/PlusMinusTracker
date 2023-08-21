db92f39b71b1de0a36b65b3939bb2856ff731708
Rob Mackowiak <robert.mackowiak@mlse.com>
2023-08-20 20:46:56 -0400

    bug test

--- app_test.py
+++ app_test.py
@@ -4,7 +4,7 @@ from datetime import date
 import numpy as np
 import csv
 import os.path
-import streamlit_toggle as tog
+#import streamlit_toggle as tog
 
 st.markdown("<h1 style='text-align: center; color: black;'>Plus-Minus Tracker</h1>", unsafe_allow_html=True)
 top_col1,top_col2,top_col3,top_col4 = st.columns([1,1,1,1])
@@ -78,26 +78,26 @@ else:
     top_col3.button('Invalid Player #',key=1)
 
 
-with top_col4:
-    table_switch = tog.st_toggle_switch(label="", 
-                    key="Key1", 
-                    default_value=True, 
-                    label_after = False, 
-                    inactive_color = '#11567f', 
-                    active_color="#11567f", 
-                    track_color="#11567f"
-                    )
+# with top_col4:
+#     table_switch = tog.st_toggle_switch(label="", 
+#                     key="Key1", 
+#                     default_value=True, 
+#                     label_after = False, 
+#                     inactive_color = '#11567f', 
+#                     active_color="#11567f", 
+#                     track_color="#11567f"
+#                     )
 
-if table_switch==True:
-    col3.subheader('TMU')
-    if 'home_points' in st.session_state:
-        height = int(35.2*(len(homeplayers)+1))
-        col3.dataframe(st.session_state.home_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True,height=height)
-else:
-    col3.subheader('Away')
-    if 'away_points' in st.session_state:
-        height = int(35.2*(len(awayplayers)+1))
-        col3.dataframe(st.session_state.away_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True,height=height)
+# if table_switch==True:
+#     col3.subheader('TMU')
+#     if 'home_points' in st.session_state:
+#         height = int(35.2*(len(homeplayers)+1))
+#         col3.dataframe(st.session_state.home_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True,height=height)
+# else:
+#     col3.subheader('Away')
+#     if 'away_points' in st.session_state:
+#         height = int(35.2*(len(awayplayers)+1))
+#         col3.dataframe(st.session_state.away_points.items(),hide_index=True,column_config={'0':'Player','1':'+/-'},use_container_width=True,height=height)
 
 st.text('')
 if st.button('Show/Update Lineup +/-'):
