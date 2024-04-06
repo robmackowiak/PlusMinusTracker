9450e99bbcba06b36951e0c616db166426f56357
Rob Mackowiak <robert.mackowiak@mlse.com>
2024-04-06 18:46:00 -0400

    reformat with Black

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import os.path
 import os
-from plusminus.utils.utils import get_roster, get_logo, custom_footer
+from plusminus.utils.utils import custom_footer
 from streamlit_js_eval import streamlit_js_eval
 
 st.set_page_config(
@@ -14,15 +12,6 @@ st.set_page_config(
 )
 
 custom_footer()
-
-@st.cache_data(ttl=3600)
-def get_rosters(team_1_url, team_2_url):
-    team1_df = get_roster(team_1_url)
-    team2_df = get_roster(team_2_url)
-    get_logo(team_1_url)
-    get_logo(team_2_url)
-    return team1_df, team2_df
-
 
 if "data" not in st.session_state:
     st.session_state["data"] = pd.DataFrame({"team": [], "name": [], "val": []})
@@ -45,64 +34,49 @@ if "data_lineup" not in st.session_state:
 with st.sidebar:
     with st.expander(f"Upload Rosters"):
         team1_col, team2_col = st.columns(2)
-        st.session_state["team1_upload"] = team1_col.file_uploader(
-            "Team 1", type=["csv"]
-        )
+        st.session_state["team1_upload"] = team1_col.file_uploader("Team 1", type=["csv"])
 
-        st.session_state["team2_upload"] = team2_col.file_uploader(
-            "Team 2", type=["csv"]
-        )
+        st.session_state["team2_upload"] = team2_col.file_uploader("Team 2", type=["csv"])
 
         if st.session_state["team1_upload"] is not None and st.session_state["team2_upload"] is not None:
-            team1 = st.session_state["team1_upload"].name.split('.')[0]
-            team2 = st.session_state["team2_upload"].name.split('.')[0]
+            team1 = st.session_state["team1_upload"].name.split(".")[0]
+            team2 = st.session_state["team2_upload"].name.split(".")[0]
 
             team1_df = pd.read_csv(st.session_state["team1_upload"])
             team2_df = pd.read_csv(st.session_state["team2_upload"])
-            
-            st.caption("Data Uploaded Succesfully ✅")
 
+            st.caption("Data Uploaded Succesfully ✅")
 
     team1_players_col, team2_players_col = st.columns(2)
 
     if st.session_state["team1_upload"] is not None and st.session_state["team2_upload"] is not None:
         with team1_players_col:
-            st.caption(team1.replace('_', ' ').title())
-            team1_active = [
-                team1_df["Player"][i]
-                for i in range(len(team1_df["Player"]))
-                if st.checkbox(f'#{team1_df["#"][i]} {team1_df["Player"][i]}')
-            ]
+            st.caption(team1.replace("_", " ").title())
+            team1_active = [team1_df["Player"][i] for i in range(len(team1_df["Player"])) if st.checkbox(f'#{team1_df["#"][i]} {team1_df["Player"][i]}')]
 
         with team2_players_col:
-            st.caption(team2.replace('_', ' ').title())
-            team2_active = [
-                team2_df["Player"][i]
-                for i in range(len(team2_df["Player"]))
-                if st.checkbox(f'#{team2_df["#"][i]} {team2_df["Player"][i]}', key=f'{team2_df["Player"][i]}')
-            ]
+            st.caption(team2.replace("_", " ").title())
+            team2_active = [team2_df["Player"][i] for i in range(len(team2_df["Player"])) if st.checkbox(f'#{team2_df["#"][i]} {team2_df["Player"][i]}', key=f'{team2_df["Player"][i]}')]
 
-        st.download_button('Download Player Data', data=st.session_state["data"].to_csv(index=False), file_name=f"{team1.replace('-','')}_{team2.replace('-','')}_plusminus_player_data.csv", use_container_width=True)
-        st.download_button('Download Lineup Data', data=st.session_state["data_lineup"].to_csv(index=False), file_name=f"{team1.replace('-','')}_{team2.replace('-','')}_plusminus_lineup_data.csv", use_container_width=True)
+        st.download_button("Download Player Data", data=st.session_state["data"].to_csv(index=False), file_name=f"{team1.replace('-','')}_{team2.replace('-','')}_plusminus_player_data.csv", use_container_width=True)
+        st.download_button("Download Lineup Data", data=st.session_state["data_lineup"].to_csv(index=False), file_name=f"{team1.replace('-','')}_{team2.replace('-','')}_plusminus_lineup_data.csv", use_container_width=True)
 
-        if st.button('Reset App', use_container_width=True, type='primary'):
+        if st.button("Reset App", use_container_width=True, type="primary"):
             streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
 if st.session_state["team1_upload"] is None or st.session_state["team2_upload"] is None:
-    st.caption(
-        "Please use sidebar to initialize two team rosters to start plus-minus tracking."
-    )
+    st.caption("Please use sidebar to initialize two team rosters to start plus-minus tracking.")
 else:
     _, logo1_col, _, _, logo2_col, _ = st.columns([1, 1, 0.5, 0.5, 1, 1])
     try:
         logo1_col.image(f"./plusminus/assets/logo/{team1}.png", use_column_width=True)
     except:
-        logo1_col.header(team1.replace('-', ''))
-    
+        logo1_col.header(team1.replace("-", ""))
+
     try:
         logo2_col.image(f"./plusminus/assets/logo/{team2}.png", use_column_width=True)
     except:
-        logo2_col.header(team2.replace('-', ''))
+        logo2_col.header(team2.replace("-", ""))
 
     (
         _,
@@ -128,9 +102,7 @@ else:
                     -1,
                 ]
 
-            st.session_state["data_lineup"].loc[
-                len(st.session_state["data_lineup"].index)
-            ] = [
+            st.session_state["data_lineup"].loc[len(st.session_state["data_lineup"].index)] = [
                 team1,
                 team1_active[0],
                 team1_active[1],
@@ -139,9 +111,7 @@ else:
                 team1_active[4],
                 1,
             ]
-            st.session_state["data_lineup"].loc[
-                len(st.session_state["data_lineup"].index)
-            ] = [
+            st.session_state["data_lineup"].loc[len(st.session_state["data_lineup"].index)] = [
                 team2,
                 team2_active[0],
                 team2_active[1],
@@ -166,9 +136,7 @@ else:
                     -1,
                 ]
 
-            st.session_state["data_lineup"].loc[
-                len(st.session_state["data_lineup"].index)
-            ] = [
+            st.session_state["data_lineup"].loc[len(st.session_state["data_lineup"].index)] = [
                 team1,
                 team1_active[0],
                 team1_active[1],
@@ -177,9 +145,7 @@ else:
                 team1_active[4],
                 -1,
             ]
-            st.session_state["data_lineup"].loc[
-                len(st.session_state["data_lineup"].index)
-            ] = [
+            st.session_state["data_lineup"].loc[len(st.session_state["data_lineup"].index)] = [
                 team2,
                 team2_active[0],
                 team2_active[1],
@@ -192,25 +158,13 @@ else:
     player, lineup = st.tabs(["Player", "Lineup"])
 
     with player:
-        _, player_col1_title, _, _, player_col2_title, _ = st.columns(
-            [1.5, 1, 0.65, 0.5, 1, 1]
-        )
-        player_col1_title.caption(team1.replace('_', ' ').title())
-        player_col2_title.caption(team2.replace('_', ' ').title())
+        _, player_col1_title, _, _, player_col2_title, _ = st.columns([1.5, 1, 0.65, 0.5, 1, 1])
+        player_col1_title.caption(team1.replace("_", " ").title())
+        player_col2_title.caption(team2.replace("_", " ").title())
 
-        _, player_col1, player_col1_val, player_col2, player_col2_val, _ = st.columns(
-            [1.2, 1.8, 1.5, 1.8, 1.5, 0.2]
-        )
+        _, player_col1, player_col1_val, player_col2, player_col2_val, _ = st.columns([1.2, 1.8, 1.5, 1.8, 1.5, 0.2])
 
-        ordered_names = (
-            st.session_state["data"]
-            .query("team==@team1")
-            .groupby(by="name")["val"]
-            .sum()
-            .sort_values(ascending=False)
-            .keys()
-            .values
-        )
+        ordered_names = st.session_state["data"].query("team==@team1").groupby(by="name")["val"].sum().sort_values(ascending=False).keys().values
         for name in ordered_names:
             number = team1_df[team1_df["Player"] == name]["#"].values[0]
             val = st.session_state["data"].query("name==@name")["val"].sum()
@@ -218,15 +172,7 @@ else:
             player_col1.write(f"#{number} {name}")
             player_col1_val.write(f"{val}")
 
-        ordered_names = (
-            st.session_state["data"]
-            .query("team==@team2")
-            .groupby(by="name")["val"]
-            .sum()
-            .sort_values(ascending=False)
-            .keys()
-            .values
-        )
+        ordered_names = st.session_state["data"].query("team==@team2").groupby(by="name")["val"].sum().sort_values(ascending=False).keys().values
         for name in ordered_names:
             number = team2_df[team2_df["Player"] == name]["#"].values[0]
             val = st.session_state["data"].query("name==@name")["val"].sum()
@@ -241,9 +187,7 @@ else:
             st.dataframe(
                 st.session_state["data_lineup"]
                 .query(f'team=="{team1}"')
-                .groupby(by=["player1", "player2", "player3", "player4", "player5"])[
-                    "val"
-                ]
+                .groupby(by=["player1", "player2", "player3", "player4", "player5"])["val"]
                 .sum()
                 .sort_values(ascending=False)
                 .reset_index()
@@ -265,9 +209,7 @@ else:
             st.dataframe(
                 st.session_state["data_lineup"]
                 .query(f'team=="{team2}"')
-                .groupby(by=["player1", "player2", "player3", "player4", "player5"])[
-                    "val"
-                ]
+                .groupby(by=["player1", "player2", "player3", "player4", "player5"])["val"]
                 .sum()
                 .sort_values(ascending=False)
                 .reset_index()
--- plusminus/utils/utils.py
+++ plusminus/utils/utils.py
@@ -1,51 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import ssl
 import streamlit as st
-
-
-def get_roster(url):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
-    # Replace 'url' with the URL of the webpage containing the HTML table
-    url = url+'#roster'
-
-    # Read HTML table into a list of DataFrame objects
-    tables = pd.read_html(url)
-
-    # If there are multiple tables on the webpage, you can access each table using indexing
-    # For example, to access the first table:
-    df = tables[0]
-    
-    # Now you have the table data in a pandas DataFrame
-    return df
-
-
-def get_logo(url):
-
-    team_name = url.split('/')[5]
-
-    # Send a GET request to the URL
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Find all elements with class="teamlogo"
-        teamlogos = soup.find_all(class_="teamlogo")
-
-        # Print or do something with the found elements
-        for logo in teamlogos:
-            img_src = logo['src']
-        
-        img_data = requests.get(img_src).content
-        with open(f'./plusminus/assets/logo/{team_name}.jpg', 'wb') as handler:
-            handler.write(img_data)
-    else:
-        print('Failed to retrieve the webpage. Status code:', response.status_code)
 
 
 def custom_footer():
@@ -64,4 +17,4 @@ def custom_footer():
                 }
             </style>
             """
-    return st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    return st.markdown(hide_streamlit_style, unsafe_allow_html=True)
